6a76264bc1e57c70f07cc9aa827c2bafe7f9a584
mchilders14 <childersmatt4@gmail.com>
2021-07-26 01:59:50 -0400

    add

--- example-objects-arrays-data-structures/js/objects.js
+++ example-objects-arrays-data-structures/js/objects.js
@@ -5,6 +5,29 @@ function helloworld() {
     console.log(hello);
 }
 
+let animal = {
+    name: "default",
+    eat: false,
+    isAlive: true
+}
+
+let shape = {
+    color: "blue",
+    name: "default name",
+    area: 1,
+    volume: 1
+}
+
+
+
+
+
+
+
+
+
+
+
 var attempt = 3; // Variable to count number of attempts.
 
 // Below function Executes on click of login button.
@@ -15,7 +38,7 @@ function validate() {
     var password = document.getElementById("password").value;
 
     //Successful Login
-    if (username == "Formget" && password == "formget#123") {
+    if (username == "user" && password == "password") {
         alert("Login successfully");
         window.location = "success.html"; // Redirecting to other page.
         return false;
